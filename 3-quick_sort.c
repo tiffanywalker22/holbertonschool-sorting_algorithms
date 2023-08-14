e884bd1c76559f01537bb3488f4fee7bf5d91db7
tiffanywalker22 <tbcasper16@gmail.com>
2023-08-14 12:53:54 -0700

    working on number 2 errors

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -12,61 +12,67 @@
 
 int partition(int *array, size_t size, int low, int high)
 {
-	int pivot = array[high];
-	int i = low - 1;
-	int j = 0;
-	int temp = 0;
+	int i = 0, j = 0, pivot = 0, aux = 0;
 
-	for (j = low; j <= high - 1; j++)
+	pivot = array[high];
+	i = low;
+
+	for (j = low; j < high; ++j)
 	{
 		if (array[j] < pivot)
 		{
-			i++;
-			temp = array[j];
-			array[j] = array[i];
-			array[i] = temp;
-			print_array(array, size);
+			aux = array[i];
+			array[i] = array[j];
+			array[j] = aux;
+
+			if (aux != array[i])
+				print_array(array, size);
+
+			++i;
 		}
 	}
-	temp = array[high];
-	array[high] = array[i + 1];
-	array[i + 1] = temp;
 
-	return (i + 1);
+	aux = array[i];
+	array[i] = array[high];
+	array[high] = aux;
+
+	if (aux != array[i])
+		print_array(array, size);
+
+	return (i);
+}
+
+/**
+ * quick_sort_rec - uses quicksort process
+ * @array: array
+ * @size: size of array
+ * @low: low
+ * @high: high
+ */
+
+void quick_sort_rec(int *array, int low, int high, size_t size)
+{
+        int p = 0;
+
+        if (low < high)
+        {
+                p = partition(array, low, high, size);
+                quick_sort_rec(array, low, p - 1, size);
+                quick_sort_rec(array, p + 1, high, size);
+        }
 }
 
 /**
  * quicksort - quicksort
  * @array: array
- * @low: low
- * @high: high
  * @size: the size
  *
  */
 
-void quicksort(int *array, size_t size, int low, int high)
-{
-	int p;
-
-	if (low < high)
-	{
-		p = partition(array, size, low, high);
-		quicksort(array, size, low, p - 1);
-		quicksort(array, size, p + 1, high);
-	}
-}
-
-
-/**
- * quick_sort - sorts int array in ascending order using Quick sort algorithm
- * @array: array
- * @size: size of array
- */
-
 void quick_sort(int *array, size_t size)
 {
-	if (array == NULL)
+	if (!array || size < 2)
 		return;
 
-	quicksort(array, size, 0, size - 1);
+	quick_sort_rec(array, 0, size - 1, size);
 }
