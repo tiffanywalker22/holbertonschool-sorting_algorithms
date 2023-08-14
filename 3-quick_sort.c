25a474338c9982cc1909ae14158c318ba76d9bd7
tiffanywalker22 <tbcasper16@gmail.com>
2023-08-14 13:15:56 -0700

    trying new method

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,78 +1,131 @@
 #include "sort.h"
 
 /**
- * partition - partitions array
+ * quick_sort - quick sort
  * @array: array
- * @low: low
- * @high: high
- * @size: the size
- *
- * Return: index
- */
-
-int partition(int *array, size_t size, int low, int high)
-{
-	int i = 0, j = 0, pivot = 0, aux = 0;
-
-	pivot = array[high];
-	i = low;
-
-	for (j = low; j < high; ++j)
-	{
-		if (array[j] < pivot)
-		{
-			aux = array[i];
-			array[i] = array[j];
-			array[j] = aux;
-
-			if (aux != array[i])
-				print_array(array, size);
-
-			++i;
-		}
-	}
-
-	aux = array[i];
-	array[i] = array[high];
-	array[high] = aux;
-
-	if (aux != array[i])
-		print_array(array, size);
-
-	return (i);
-}
-
-/**
- * quick_sort_rec - uses quicksort process
- * @array: array
- * @size: size of array
- * @low: low
- * @high: high
- */
-
-void quick_sort_rec(int *array, int low, int high, size_t size)
-{
-        int p = 0;
-
-        if (low < high)
-        {
-                p = partition(array, low, high, size);
-                quick_sort_rec(array, low, p - 1, size);
-                quick_sort_rec(array, p + 1, high, size);
-        }
-}
-
-/**
- * quicksort - quicksort
- * @array: array
- * @size: the size
+ * @size: size
  *
  */
-
 void quick_sort(int *array, size_t size)
 {
-	if (!array || size < 2)
-		return;
+	int piv, high, low;
 
-	quick_sort_rec(array, 0, size - 1, size);
+	if (size < 2)
+		return;
+	high = (int)size - 1;
+	low = 0;
+	if (low < high)
+	{
+		piv = partition(array, low, high, size);
+		quicksort(array, low, piv - 1, size);
+		quicksort(array, piv + 1, high, size);
+	}
+}
+
+/**
+ * quicksort - sorts parts of array
+ * @array: array
+ * @low: low
+ * @high: high
+ * @size: size
+ */
+void quicksort(int *array, int low, int high, size_t size)
+{
+	int piv;
+
+	if (low < high)
+	{
+		piv = partition(array, low, high, size);
+		quicksort(array, low, piv - 1, size);
+		quicksort(array, piv + 1, high, size);
+	}
+}
+
+/**
+ * e_swap - swap elements of array
+ * @array_1: first element
+ * @array_2: second element
+ *
+ */
+void e_swap(int *array_1, int *array_2)
+{
+	int temp;
+
+	temp = *array_1;
+	*array_1 = *array_2;
+	*array_2 = temp;
+}
+
+
+/**
+ * partition - makes partition
+ * @array: array
+ * @low: low
+ * @high: high
+ * @size: size
+ *
+ * Return: index
+ *
+ */
+int partition(int *array, int low, int high, size_t size)
+{
+	int index, pivot, index2;
+
+	pivot = array[high];
+	index = (low - 1);
+	for (index2 = low; index2 <= high - 1; index2++)
+	{
+		if (array[index2] <= pivot)
+		{
+			index++;
+			if (index != index2)
+			{
+				e_swap(&array[index], &array[index2]);
+				print_array(array, size);
+			}
+		}
+	}
+	if (index + 1 != high)
+	{
+		e_swap(&array[index + 1], &array[high]);
+		print_array(array, size);
+	}
+	return (index + 1);
+}
+
+/**
+ * highandlow - the high or low value based on request
+ * @array: array
+ * @size: size
+ * @h_l: flag
+ *
+ * Return: high or low value
+ */
+int highandlow(int *array, size_t size, int h_l)
+{
+	int value = array[0], index = 1, temp_size = 0;
+	int ar_index = 0;
+
+	temp_size = (int)size;
+	for (index = 1; index < temp_size; index++)
+	{
+		if (h_l == 0)
+		{
+			if (array[index] < value)
+			{
+				value = array[index];
+				ar_index = index;
+			}
+		}
+		else
+		{
+			if (array[index] > value)
+			{
+				value = array[index];
+				ar_index = index;
+			}
+		}
+		index++;
+	}
+	return (ar_index);
 }
--- sort.h
+++ sort.h
@@ -22,6 +22,7 @@ void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
-int partition(int *array, size_t size, int low, int high);
-void quick_sort(int *array, size_t size);
+void quicksort(int *array, int low, int high, size_t size);
+int partition(int *array, int low, int high, size_t size);
+int highandlow(int *array, size_t size, int h_l);
 #endif
