f807c84e3c7fb3c293bbca0363e984b0f1abfbd1
tiffanywalker22 <tbcasper16@gmail.com>
2023-08-14 12:37:33 -0700

    reworking on answer 2

--- sort.h
+++ sort.h
@@ -22,6 +22,4 @@ void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
-void quick_sort(int *array, size_t size);
-int partition(int *array, size_t size, int low, int high);
 #endif
